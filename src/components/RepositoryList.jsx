d12692e045ea116958adaceb0316089fc934331b
matheus.teixeira <matheus.teixeira@flieger.software>
2022-12-08 19:01:54 -0300

    listing-repositories-git

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,30 +2,24 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 import { useState, useEffect } from "react";
 
-const repository = {
-    name:'Nome',
-    description:'Descrição',
-    link:'https://github.com/Emelly-Plautz?tab=repositories'
-}
 
 export function RepositoryList() {
     const [repositories, setRepositories] = useState([]);
 
     useEffect(() => {
-        fetch('https://api.github.com/orgs/rockseat/repos')
+        fetch('https://api.github.com/users/Emelly-Plautz/repos')
         .then(response => response.json())
         .then(data => setRepositories(data))
     }, []);
 
     return (
         <section className="repository-list">
-            <h1>Lista de repositórios</h1>
+            <h1>List of repositories</h1>
 
             <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
+                {repositories.map(repository =>{
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     );
